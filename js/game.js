b6a3ebf4b95cb4398411064f3cfe610a8a10aeba
Oscar <oscarlopez@despegar.com>
2014-12-22 15:47:39 -0300

    Se agregan los 2 personaes y las balas

--- js/game.js
+++ js/game.js
@@ -1,5 +1,8 @@
 var TTPP = function(){
     var _this = this;
+    _this.players = {};
+    _this.bulletTime = 0;
+
 
     _this.init = function(){
         _this.heigth = 640;
@@ -23,6 +26,7 @@ var TTPP = function(){
         _this.game.load.image('grass','assets/background/light_grass.png');
         _this.game.load.image('sand','assets/background/sand.png');
         _this.game.load.image('earth','assets/background/earth.png');
+        _this.game.load.image('bullet','assets/bullet_black.png');
 
         /**
          * Players sprites
@@ -32,54 +36,102 @@ var TTPP = function(){
         /**
          * png player 3 - 1650px x 468px
          */
-        _this.game.load.spritesheet('player3','assets/player3.png',54,108,48);
+        _this.game.load.spritesheet('player3','assets/player3.png',150,117);
         _this.game.load.spritesheet('dude','assets/dude.png',32,48);
-
-
-
     }
 
     _this.create = function(){
         _this.buildWorld();
 
-        _this.createPlayer('player3');
+        _this.createPlayer('knife','player3',_this.arrowControls);
+        _this.createPlayer('shooter','player3',_this.aswdControls);
 
+
+        _this.createBullets();
         _this.buildControls();
     }
 
     _this.update = function(){
-
         _this.processActions();
+    }
+
+
+    _this.arrowControls = function(){
+        _this.player = _this.players['knife'];
+
+        if (_this.cursors.left.isDown) {
+            //  Move to the left
+            _this.player.body.velocity.x = -150;
+            _this.player.animations.play('left');
+        }
+        else if (_this.cursors.right.isDown){
+            //  Move to the right
+            _this.player.body.velocity.x = 150;
+            _this.player.animations.play('right');
+        }
+        else if(_this.cursors.down.isDown){
+            _this.player.body.velocity.y = 150;
+            _this.player.animations.play('down');
+        }
+        else if(_this.cursors.up.isDown){
+            _this.player.body.velocity.y = -150;
+            _this.player.animations.play('up');
+        }
+        else {
+            //  Stand still
+            _this.player.animations.stop();
+            _this.player.body.velocity.y = 0;
+            _this.player.body.velocity.x = 0;
+            //_this.player.frame = 1;
+        }
+
+        if(_this.cursors.fireKey.isDown){
+            //console.log('fire knifes');
+            _this.fireBullets(_this.player);
+        }
+    }
+
+    _this.aswdControls = function(){
+        _this.player = _this.players['shooter'];
+
+
+        if (_this.leftKey.isDown) {
+            //  Move to the left
+            _this.player.body.velocity.x = -150;
+            _this.player.animations.play('left');
+        }
+        else if (_this.rightKey.isDown){
+            //  Move to the right
+            _this.player.body.velocity.x = 150;
+            _this.player.animations.play('right');
+        }
+        else if(_this.downKey.isDown){
+            _this.player.body.velocity.y = 150;
+            _this.player.animations.play('down');
+        }
+        else if(_this.upKey.isDown){
+            _this.player.body.velocity.y = -150;
+            _this.player.animations.play('up');
+        }
+        else {
+            //  Stand still
+            _this.player.animations.stop();
+            _this.player.body.velocity.y = 0;
+            _this.player.body.velocity.x = 0;
+            //_this.player.frame = 1;
+        }
+        if(_this.fireKey.isDown){
+            _this.fireBullets(_this.player);
+        }
+
     }
 
     _this.processActions = function(){
         //  Reset the players velocity (movement)
         _this.player.body.velocity.x = 0;
 
-        if (_this.cursors.left.isDown) {
-            //  Move to the left
-            _this.player.body.velocity.x = -150;
-
-            _this.player.animations.play('left');
-        }else if (_this.cursors.right.isDown){
-            //  Move to the right
-            _this.player.body.velocity.x = 150;
-            //_this.player.animations.play('right');
-
-        }
-        else if(_this.cursors.down.isDown){
-            _this.player.body.velocity.y = 150;
-            _this.player.animations.play('right');
-        }
-        else if(_this.cursors.up.isDown){
-            _this.player.body.velocity.y = -150;
-            _this.player.animations.play('left');
-        }
-        else {
-            //  Stand still
-            _this.player.animations.stop();
-            _this.player.frame = 4;
-        }
+        _this.arrowControls();
+        _this.aswdControls();
 
         //  Allow the player to jump if they are touching the ground.
         //if (_this.cursors.up.isDown && _this.player.body.touching.down)
@@ -95,6 +147,9 @@ var TTPP = function(){
             _this.game.add.sprite(i * 128 ,y,element);
         }
     }
+
+
+
 
     /**
      * Creates the background
@@ -112,26 +167,69 @@ var TTPP = function(){
 
     }
 
-    _this.createPlayer = function(asset){
+    _this.createPlayer = function(name,asset,controlsFunction){
         _this.player = _this.game.add.sprite(_this.width / 2,0,asset);
         _this.game.physics.arcade.enable(_this.player);
 
         //  Player physics properties. Give the little guy a slight bounce.
         //Cuando se agrega Physics, el objeto obtiene la propiedad body para modificar parametros de physics
         //_this.player.body.bounce.y = 0.2;
-        //_this.player.body.gravity.y = 300;
+        //_this.player.body.gravity.y = 0;
         _this.player.body.collideWorldBounds = true;
+        _this.player.body.allowGravity = false;
 
         //  Our two animations, walking left and right.
-        _this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-        _this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+        _this.player.animations.add('left', [22,23, 24,25,26,27], 10, true);
+        _this.player.animations.add('right', [33,34,35,36,37,38,39], 10, true);
+        _this.player.animations.add('up', [11,12,13,14,15,16], 10, true);
+        _this.player.animations.add('down', [0,1,2,3,4,5,6], 10, true);
 
-        _this.player.animations.add('walk');
+        if(typeof _this.players[name] === "undefined"){
+            //_this.player.controls = controlsFunction;
+            _this.players[name] = _this.player;
+        }
     }
 
-
     _this.buildControls = function(){
+        _this.fireKey = _this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        _this.leftKey = _this.game.input.keyboard.addKey(65);
+        _this.rightKey = _this.game.input.keyboard.addKey(68);
+        _this.downKey = _this.game.input.keyboard.addKey(83);
+        _this.upKey = _this.game.input.keyboard.addKey(87);
+
         _this.cursors = _this.game.input.keyboard.createCursorKeys();
+        _this.cursors.fireKey = _this.game.input.keyboard.addKey(13);//Enter
+    }
+
+    _this.createBullets = function(){
+        //  Our bullet group
+        _this.bullets = _this.game.add.group();
+        _this.bullets.enableBody = true;
+        _this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        _this.bullets.createMultiple(30, 'bullet', 0, false);
+        _this.bullets.setAll('anchor.x', 0.5);
+        _this.bullets.setAll('anchor.y', 0.5);
+        _this.bullets.setAll('outOfBoundsKill', true);
+        _this.bullets.setAll('checkWorldBounds', true);
+    }
+
+    _this.fireBullets = function (player) {
+        console.log('fire bullets');
+        //  To avoid them being allowed to fire too fast we set a time limit
+        if (_this.game.time.now > _this.bulletTime)
+        {
+            //  Grab the first bullet we can from the pool
+           _this.bullet = _this.bullets.getFirstExists(false);
+
+            if (_this.bullet)
+            {
+                //  And fire it
+                _this.bullet.reset(player.x+54, player.y+30);
+                _this.bullet.body.velocity.y = -150;
+                _this.bulletTime = _this.game.time.now + 300;
+            }
+        }
+
     }
 
 }
